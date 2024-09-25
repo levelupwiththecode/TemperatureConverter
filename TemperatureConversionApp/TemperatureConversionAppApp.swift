20bd26bd8a7445211cea7ac463795b5f526815f7
Dosso <massidedosso@gmail.com>
2024-09-25 20:26:33 +0200

    Initial Commit

---
+++ TemperatureConversionApp/TemperatureConversionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConversionAppApp.swift
+//  TemperatureConversionApp
+//
+//  Created by Massidé Dosso on 25/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConversionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
